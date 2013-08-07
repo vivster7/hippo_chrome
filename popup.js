441f6951282d0e23323eb6c9bb1b4bad5a14991b
Vivek D <vivster7@gmail.com>
2013-08-07 13:58:55 -0700

    deleted popup for real

--- popup.js
+++
@@ -1,4 +0,0 @@
-//Sends background.js a message to tell content script to try
-//and reload the observer
-var backgroundWindow = chrome.extension.getBackgroundPage();
-backgroundWindow.callReloadObserver();
